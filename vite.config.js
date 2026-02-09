5b437e969f7fec6236be603ccd15bf59d6e644fe
Boxing24 Admin <admin@boxing24.pl>
2026-02-09 01:24:33 +0100

    Force Vercel Rebuild v3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Trigger restart for env reload
+  // Trigger restart for env reload - FORCE DEPLOY V3
 })
