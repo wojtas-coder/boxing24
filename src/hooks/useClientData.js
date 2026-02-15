c6c271c1b9ba8ac7ac9f7e1f63561076395da709
Boxing24 Admin <admin@boxing24.pl>
2026-02-15 01:22:17 +0100

    chore: cleanup useClientData and finalize logout fix

--- src/hooks/useClientData.js
+++ src/hooks/useClientData.js
@@ -65,5 +65,5 @@ export const useClientData = () => {
 
     const allPlans = [...plansLibrary, ...dbPlans];
 
-    return { ...clientData, allPlans, refresh: () => { } }; // Simplified for now
+    return { ...clientData, allPlans, refresh: () => { } };
 };
